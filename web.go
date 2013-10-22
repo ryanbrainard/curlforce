a2bf21e2fea5bb581262e0400525d00cee3f8aa7
Ryan Brainard <brainard@heroku.com>
2013-10-22 15:17:48 -0700

    factor out OAuthClient

--- web.go
+++ web.go
@@ -1,88 +1,90 @@
 package main
 
 import (
+    "encoding/json"
     "fmt"
+    "io/ioutil"
     "net/http"
     "net/url"
     "os"
-    "io/ioutil"
-    "encoding/json"
 )
 
 func main() {
-    http.HandleFunc("/", index)
-    http.HandleFunc("/oauth/_callback", handleCallback)
+    oauth := OAuthClient{
+        os.Getenv("SFDC_OAUTH_LOGIN_HOST"), 
+        os.Getenv("SFDC_OAUTH_CLIENT_KEY"), 
+        os.Getenv("SFDC_OAUTH_CLIENT_SECRET"), 
+        os.Getenv("SFDC_OAUTH_CALLBACK_URL"),
+    }
+
+    http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+        http.Redirect(res, req, oauth.authUrl(), http.StatusFound)
+    })
+
+    http.HandleFunc("/oauth/_callback", func(res http.ResponseWriter, req *http.Request) {
+        tokenRes, tokenErr := http.PostForm(oauth.tokenUrl(), oauth.tokenParams(req.FormValue("code")))
+        if tokenErr != nil {
+            fmt.Println(tokenErr) // TODO: handle error
+        }
+
+        defer tokenRes.Body.Close()
+        tokenResBody, err := ioutil.ReadAll(tokenRes.Body)
+        if err != nil {
+            fmt.Println(err) // TODO: handle error
+        }
+
+        var tokenResMap map[string]interface{}
+
+        if err := json.Unmarshal(tokenResBody, &tokenResMap); err != nil {
+            fmt.Println(err) // TODO: handle error
+        }
+
+        fmt.Fprintln(res, output(tokenResMap["instance_url"].(string), tokenResMap["access_token"].(string)))
+    })
+
     fmt.Println("listening...")
     err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
     if err != nil {
-      panic(err)
+        panic(err)
     }
 }
 
-func index(res http.ResponseWriter, req *http.Request) {
-    // TODO state param
-    authUrl := 
-        "https://" + 
-        loginHost() + 
-        "/services/oauth2/authorize?response_type=code&display=popup" + 
-        "&client_id=" + url.QueryEscape(clientKey()) + 
-        "&redirect_uri=" + url.QueryEscape(callbackUrl())
-
-    http.Redirect(res, req, authUrl, http.StatusFound)
+type OAuthClient struct {
+    loginHost string
+    clientKey string
+    clientSecret string
+    callbackUrl string    
 }
 
-func handleCallback(res http.ResponseWriter, req *http.Request) {
-    tokenUrl := "https://" + loginHost() + "/services/oauth2/token"
-
-    v := url.Values{}
-    v.Set("code", req.FormValue("code"))
-    v.Set("grant_type", "authorization_code")
-    v.Set("client_id", clientKey())
-    v.Set("client_secret", clientSecret())
-    v.Set("redirect_uri", callbackUrl())
-
-    tokenRes, tokenErr := http.PostForm(tokenUrl, v)
-    if tokenErr != nil {
-        fmt.Println(tokenErr) // TODO: handle error
-    }
-
-    defer tokenRes.Body.Close()
-    tokenResBody, err := ioutil.ReadAll(tokenRes.Body)
-    if err != nil {
-        fmt.Println(err)  // TODO: handle error
-    }
-
-    var tokenResMap map[string]interface{}
-
-    if err := json.Unmarshal(tokenResBody, &tokenResMap); err != nil {
-        fmt.Println(err)  // TODO: handle error
-    }
-
-    fmt.Fprintln(res, "export SFDC_INSTANCE_URL='" + tokenResMap["instance_url"].(string) + "'")
-    fmt.Fprintln(res, "export SFDC_ACCESS_TOKEN='" + tokenResMap["access_token"].(string) + "'")
-
-    fmt.Fprintln(res, "")
-    fmt.Fprintln(res, "curl -H 'X-PrettyPrint: 1' -H \"Authorization: Bearer $SFDC_ACCESS_TOKEN\" $SFDC_INSTANCE_URL/services/data")
-
-    fmt.Fprintln(res, "")
-    fmt.Fprintln(res, "sudo gem install restforce")
-    fmt.Fprintln(res, "irb")
-    fmt.Fprintln(res, "require 'restforce'")
-    fmt.Fprintln(res, "client = Restforce.new(:oauth_token => ENV['SFDC_ACCESS_TOKEN'], :instance_url  => ENV['SFDC_INSTANCE_URL'])")
+func (c OAuthClient) authUrl() string {
+    return "https://" + c.loginHost +
+           "/services/oauth2/authorize?response_type=code&display=popup" +
+           "&client_id=" + url.QueryEscape(c.clientKey) +
+           "&redirect_uri=" + url.QueryEscape(c.callbackUrl)
 }
 
-func loginHost() string {
-    return os.Getenv("SFDC_OAUTH_LOGIN_HOST")
+func (c OAuthClient) tokenUrl() string {
+    return "https://" + c.loginHost + "/services/oauth2/token"
 }
 
-func clientKey() string {
-    return os.Getenv("SFDC_OAUTH_CLIENT_KEY")
+func (oauth OAuthClient) tokenParams(code string) url.Values {
+    p := url.Values{}
+    p.Set("code", code)
+    p.Set("grant_type", "authorization_code")
+    p.Set("client_id", oauth.clientKey)
+    p.Set("client_secret", oauth.clientSecret)
+    p.Set("redirect_uri", oauth.callbackUrl)
+    return p
 }
 
-func clientSecret() string {
-    return os.Getenv("SFDC_OAUTH_CLIENT_SECRET")
-}
-
-func callbackUrl() string {
-    return os.Getenv("SFDC_OAUTH_CALLBACK_URL")
+func output(instanceUrl string, accessToken string) string {
+    return  "export SFDC_INSTANCE_URL='" + instanceUrl + "'\n" +
+            "export SFDC_ACCESS_TOKEN='" +  accessToken + "'\n" +
+            "\n" +
+            "curl -H 'X-PrettyPrint: 1' -H \"Authorization: Bearer $SFDC_ACCESS_TOKEN\" $SFDC_INSTANCE_URL/services/data\n" +
+            "\n" +
+            "sudo gem install restforce\n" +
+            "irb\n" +
+            "require 'restforce'\n" +
+            "client = Restforce.new(:oauth_token => ENV['SFDC_ACCESS_TOKEN'], :instance_url  => ENV['SFDC_INSTANCE_URL'])"
 }
