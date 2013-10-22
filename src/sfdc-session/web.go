43cecf59bd91e9f82cd14942ead869dc36f79184
Ryan Brainard <brainard@heroku.com>
2013-10-21 19:53:47 -0700

    redirect and fake callback

--- src/sfdc-session/web.go
+++ src/sfdc-session/web.go
@@ -3,11 +3,13 @@ package main
 import (
     "fmt"
     "net/http"
+    "net/url"
     "os"
 )
 
 func main() {
-    http.HandleFunc("/", hello)
+    http.HandleFunc("/", index)
+    http.HandleFunc("/oauth/_callback", handleCallback)
     fmt.Println("listening...")
     err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
     if err != nil {
@@ -15,6 +17,18 @@ func main() {
     }
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(res, "hello, world")
+func index(res http.ResponseWriter, req *http.Request) {
+    // TODO state param
+    authUrl := 
+        "https://" + 
+        os.Getenv("SFDC_OAUTH_LOGIN_HOST") + 
+        "/services/oauth2/authorize?response_type=code&display=popup" + 
+        "&client_id=" + url.QueryEscape(os.Getenv("SFDC_OAUTH_CLIENT_KEY")) + 
+        "&redirect_uri=" + url.QueryEscape(os.Getenv("SFDC_OAUTH_CALLBACK_URL"))
+
+    http.Redirect(res, req, authUrl, http.StatusFound)
+}
+
+func handleCallback(res http.ResponseWriter, req *http.Request) {
+    fmt.Fprintln(res, "CALLED BACK")
 }
